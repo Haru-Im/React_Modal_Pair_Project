ba052322d0e7d6a687e3cc946d3f0ecb17919783
Haru Im <chalk323@naver.com>
2023-07-07 18:37:15 +0900

    Feat: Implement input component and related logic :wq
    :wq
    
    :wq
    :wq
    :wq

---
+++ src/pages/modals/input/input-component/input.component.jsx
@@ -0,0 +1,27 @@
+import { Button, Input, Label } from "../input-style/input.style";
+import { INPUT_NAME, INPUT_PRICE } from "../input-utils";
+
+export const InputComponent = ({ attribute, value, onChange }) => {
+  let labelText = "";
+  switch (attribute) {
+    case INPUT_NAME:
+      labelText = "이름";
+      break;
+    case INPUT_PRICE:
+      labelText = "가격";
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div>
+      <label>{labelText}</label>
+      <Input value={value} onChange={onChange} />
+    </div>
+  );
+};
+
+export const ButtonComponent = ({ children, onClick }) => {
+  return <Button onClick={onClick}>{children}</Button>;
+};
