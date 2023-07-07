dcc959be9366f506512875a6ae7ff2c0b427158a
mmms2000 <moseskmcmmms@gmail.com>
2023-07-07 10:47:23 +0900

    Feat: add Buttons

---
+++ src/components/containers/buttoncotainer.jsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const ButtonContainer = styled.div`
+margin-left: 10px;
+display: flex;
+flex-direction: row;
+gap: 10px;
+margin-bottom: 10px;
+`
