85291391f118550d99103a22fbbd0f3c1522f7fb
Haru Im <chalk323@naver.com>
2023-07-07 15:56:50 +0900

    Chore: Add button related constants and icon utils

---
+++ src/modals/components/button/button-utils/constant.js
@@ -0,0 +1,5 @@
+export const ATTRIBUTE_PRIMARY = "primary";
+export const ATTRIBUTE_NEGATIVE = "negative";
+export const SIZE_LARGE = "large";
+export const SIZE_MEDIUM = "medium";
+export const SIZE_SMALL = "small";
