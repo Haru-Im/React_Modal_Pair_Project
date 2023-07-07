172d8d92374e2cd06171bcdd3e129685fb7dadc0
Haru Im <chalk323@naver.com>
2023-07-07 15:57:56 +0900

    Feat: Create button event handlers

---
+++ src/modals/components/button/button-handler/button.handler.js
@@ -0,0 +1,9 @@
+import { ATTRIBUTE_NEGATIVE, ATTRIBUTE_PRIMARY } from "../button-utils";
+
+export const handleClick = (attribute) => {
+  if (attribute === ATTRIBUTE_PRIMARY) {
+    alert("Primary button clicked!");
+  } else if (attribute === ATTRIBUTE_NEGATIVE) {
+    alert("Primary button clicked!");
+  }
+};
