0b6e22f343a9a3374425d7d098cbdeed89d1172e
Haru Im <chalk323@naver.com>
2023-07-07 18:30:01 +0900

    Refactor: Extract global styles from specific component for reusability

---
+++ src/pages/modals/styles/sharedStyles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  display: block;
+  font-size: 2em;
+  margin-block-start: 0.67em;
+  margin-block-end: 0.67em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+`;
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
