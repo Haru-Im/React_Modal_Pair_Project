5fc3a8b45729e976658271a7d2b928706bcd23c0
Haru Im <chalk323@naver.com>
2023-07-07 18:35:13 +0900

    Feat: Implement input layout and related logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "styled-components";
-import { ButtonLayout, theme } from "./modals";
+import { ButtonLayout, InputLayout, theme } from "./pages";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <ButtonLayout />
+      <InputLayout />
     </ThemeProvider>
   );
 }
---
+++ src/pages/modals/input/input-layout/input.layout.jsx
@@ -0,0 +1,11 @@
+import { Layout, Title } from "../../styles";
+import { InputContainer } from "../input-container";
+
+export const InputLayout = ({}) => {
+  return (
+    <Layout>
+      <Title>Input</Title>
+      <InputContainer />
+    </Layout>
+  );
+};
