96685f01cf259f3ee313ae0bb9da3b9076624a02
Haru Im <chalk323@naver.com>
2023-07-10 19:06:15 +0900

    Feat: Implement modal page and related logic

--- src/pages/modals/modals.page.jsx
+++ src/pages/modals/modals.page.jsx
@@ -1,11 +1,13 @@
 import { ButtonPage } from "./button";
 import { InputPage } from "./input";
+import { ModalPage } from "./modal";
 
 export const ModalsPage = ({}) => {
   return (
     <>
-      <ButtonPage />;
+      <ButtonPage />
       <InputPage />
+      <ModalPage />
     </>
   );
 };
